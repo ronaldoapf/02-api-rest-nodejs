a5da09f153ed96b6407f6d8f4d8113b122ee18f6
ronaldoapf <ronaldo.alves.1997@gmail.com>
2026-01-18 16:28:55 -0300

    feat: configure sqlit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,12 @@
 import fastify from 'fastify'
+import { knex } from './database'
 
 const app = fastify()
 
 app.get('/', async (request, reply) => {
-  return { message: 'Hello, World!' }
+  const tables = await knex('sqlite_schema').select('*')
+
+  return tables
 })
 
 app
