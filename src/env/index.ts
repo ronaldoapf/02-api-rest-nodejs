11d7205a26ca36682f05bacaac94e983d14320df
ronaldoapf <ronaldo.alves.1997@gmail.com>
2026-01-26 17:15:11 -0300

    feat: create env validation using zod

---
+++ src/env/index.ts
@@ -0,0 +1,20 @@
+import z from 'zod'
+import 'dotenv/config'
+
+const envSchema = z.object({
+  NODE_ENV: z
+    .enum(['development', 'production', 'test'])
+    .default('development'),
+  DATABASE_URL: z.string(),
+  PORT: z.coerce.number().default(3333),
+})
+
+const _env = envSchema.safeParse(process.env)
+
+if (_env.success === false) {
+  console.error('⚠️ Invalid environment variables', z.treeifyError(_env.error))
+
+  throw new Error('Invalid environment variables.')
+}
+
+export const env = _env.data
