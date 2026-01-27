48e1cac3734afef3fabeb1def6dd77e28e85a9b7
ronaldoapf <ronaldo.alves.1997@gmail.com>
2026-01-27 14:05:09 -0300

    feat: use pg as database too

--- src/env/index.ts
+++ src/env/index.ts
@@ -11,6 +11,7 @@ const envSchema = z.object({
   NODE_ENV: z
     .enum(['development', 'production', 'test'])
     .default('development'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
   PORT: z.coerce.number().default(3333),
 })
